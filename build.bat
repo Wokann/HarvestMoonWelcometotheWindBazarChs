2cc1f9067361cb4d9b295111bdb269fc017537a3
GitHub Wokann <Kenny_chen1998@outlook.com>
2023-11-02 11:59:02 +0800

    完整批处理命令

--- build.bat
+++ build.bat
@@ -8,19 +8,19 @@ md .temp
 .\tools\armips.exe main_HM_WTTWB_Chs.asm
 ::
 ::打包汉化nds文件
-::.\tools\ndstool.exe -c .\HM_WTTWB_Chs_main.nds -9 .\.temp\arm9.bin -7 .\.temp\arm7.bin -y9 .\.temp\y9.bin -y7 .\.temp\y7.bin -d .\.temp\data -y .\.temp\overlay -t .\.temp\banner.bin -h .\.temp\header.bin
+.\tools\ndstool.exe -c .\HM_WTTWB_Chs_main.nds -9 .\.temp\arm9.bin -7 .\.temp\arm7.bin -y9 .\.temp\y9.bin -y7 .\.temp\y7.bin -d .\.temp\data -y .\.temp\overlay -t .\.temp\banner.bin -h .\.temp\header.bin
 ::
 ::替换中文语音
-::.\tools\armips.exe Optional_ChineseVoice.asm
+.\tools\armips.exe Optional_ChineseVoice.asm
 ::
 ::打包汉化nds文件（中文语音版）
-::.\tools\ndstool.exe -c .\HM_WTTWB_Chs_chsvoice.nds -9 .\.temp\arm9.bin -7 .\.temp\arm7.bin -y9 .\.temp\y9.bin -y7 .\.temp\y7.bin -d .\.temp\data -y .\.temp\overlay -t .\.temp\banner.bin -h .\.temp\header.bin
+.\tools\ndstool.exe -c .\HM_WTTWB_Chs_chsvoice.nds -9 .\.temp\arm9.bin -7 .\.temp\arm7.bin -y9 .\.temp\y9.bin -y7 .\.temp\y7.bin -d .\.temp\data -y .\.temp\overlay -t .\.temp\banner.bin -h .\.temp\header.bin
 ::
 ::填充nds文件对齐整数倍
 .\tools\armips.exe .\tools\Fill.asm
 ::
 ::清理临时文件夹
-::rd /s /q .\.temp
+rd /s /q .\.temp
 ::
 ::3秒后自动关闭本窗口
 @echo off
