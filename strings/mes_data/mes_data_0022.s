733a3d28c0769fb712f4bcb76f4299a243f6ac82
GitHub Wokann <Kenny_chen1998@outlook.com>
2023-11-02 12:06:44 +0800

    mes_data排版

--- strings/mes_data/mes_data_0022.s
+++ strings/mes_data/mes_data_0022.s
@@ -7,45 +7,88 @@ mes_data_0022_header:
     .hword (mes_data_0022_0024 - mes_data_0022_header), (mes_data_0022_0025 - mes_data_0022_header), (mes_data_0022_0026 - mes_data_0022_header), (mes_data_0022_0027 - mes_data_0022_header), (mes_data_0022_0028 - mes_data_0022_header), (mes_data_0022_0029 - mes_data_0022_header), (mes_data_0022_0030 - mes_data_0022_header), (mes_data_0022_0031 - mes_data_0022_header) 
     .hword (mes_data_0022_0032 - mes_data_0022_header), (mes_data_0022_0033 - mes_data_0022_header), (mes_data_0022_0034 - mes_data_0022_header), (mes_data_0022_0035 - mes_data_0022_header), (mes_data_0022_0036 - mes_data_0022_header), (mes_data_0022_0037 - mes_data_0022_header), (mes_data_0022_0038 - mes_data_0022_header)
 
-mes_data_0022_0000: .strn "请触摸画面{结束}"
-mes_data_0022_0001: .strn "开始新游戏{结束}"
-mes_data_0022_0002: .strn "继续游戏{结束}"
-mes_data_0022_0003: .strn "通信{结束}"
-mes_data_0022_0004: .strn "没有数据{结束}"
-mes_data_0022_0005: .strn "{名字}{结束}"
-mes_data_0022_0006: .strn "{3023}{结束}"
-mes_data_0022_0007: .strn "使用哪个存档开始游戏？{结束}"
-mes_data_0022_0008: .strn "这里这么写么？{结束}"
-mes_data_0022_0009: .strn "是的{结束}"
-mes_data_0022_0010: .strn "不要{结束}"
-mes_data_0022_0011: .strn "读取哪个记录呢{结束}"
-mes_data_0022_0012: .strn "读取这个记录么？{结束}"
-mes_data_0022_0013: .strn "读取中。{结束}"
-mes_data_0022_0014: .strn "请不要切断电源，{2408}{3308}或者{结束}"
-mes_data_0022_0015: .strn "将卡带拔出。{结束}"
-mes_data_0022_0016: .strn "保存记录中。{结束}"
-mes_data_0022_0017: .strn "保存完成。{结束}"
-mes_data_0022_0018: .strn "数据无法读取。{结束}"
-mes_data_0022_0019: .strn "请切断电源，{2408}{3308}并重新{结束}"
-mes_data_0022_0020: .strn "插入卡带。{结束}"
-mes_data_0022_0021: .strn "１号存档记录可能损坏{↙}无法读取。{↙}{结束}"
-mes_data_0022_0022: .strn "消除１号存档记录。{结束}"
-mes_data_0022_0023: .strn "２号存档记录可能损坏{↙}无法读取。{↙}{结束}"
-mes_data_0022_0024: .strn "消除２号存档记录。{结束}"
-mes_data_0022_0025: .strn "数据无法读取。{结束}"
-mes_data_0022_0026: .strn "Ｗｉ－Ｆｉ设定{结束}"
-mes_data_0022_0027: .strn "Ｗｉ－Ｆｉ设定{结束}"
-mes_data_0022_0028: .strn "Ｗｉ－Ｆｉ设定{结束}"
-mes_data_0022_0029: .strn "Ｗｉ－Ｆｉ设定{结束}"
-mes_data_0022_0030: .strn "Ｗｉ－Ｆｉ设定{结束}"
-mes_data_0022_0031: .strn "１号存档记录可能损坏{结束}"
-mes_data_0022_0032: .strn "无法读取。{结束}"
-mes_data_0022_0033: .strn "消除１号存档记录。{结束}"
-mes_data_0022_0034: .strn "２号存档记录可能损坏{结束}"
-mes_data_0022_0035: .strn "无法读取。{结束}"
-mes_data_0022_0036: .strn "消除２号存档记录。{结束}"
-mes_data_0022_0037: .strn "还无法进行通信的文件…{结束}"
-mes_data_0022_0038: .strn "　没有数据的文件　{结束}"
+mes_data_0022_0000:
+    .strn "请触摸画面{结束}"
+mes_data_0022_0001:
+    .strn "开始新游戏{结束}"
+mes_data_0022_0002:
+    .strn "继续游戏{结束}"
+mes_data_0022_0003:
+    .strn "通信{结束}"
+mes_data_0022_0004:
+    .strn "没有数据{结束}"
+mes_data_0022_0005:
+    .strn "{名字}{结束}"
+mes_data_0022_0006:
+    .strn "{3023}{结束}"
+mes_data_0022_0007:
+    .strn "使用哪个存档开始游戏？{结束}"
+mes_data_0022_0008:
+    .strn "这里这么写么？{结束}"
+mes_data_0022_0009:
+    .strn "是的{结束}"
+mes_data_0022_0010:
+    .strn "不要{结束}"
+mes_data_0022_0011:
+    .strn "读取哪个记录呢{结束}"
+mes_data_0022_0012:
+    .strn "读取这个记录么？{结束}"
+mes_data_0022_0013:
+    .strn "读取中。{结束}"
+mes_data_0022_0014:
+    .strn "请不要切断电源，{2408}{3308}或者{结束}"
+mes_data_0022_0015:
+    .strn "将卡带拔出。{结束}"
+mes_data_0022_0016:
+    .strn "保存记录中。{结束}"
+mes_data_0022_0017:
+    .strn "保存完成。{结束}"
+mes_data_0022_0018:
+    .strn "数据无法读取。{结束}"
+mes_data_0022_0019:
+    .strn "请切断电源，{2408}{3308}并重新{结束}"
+mes_data_0022_0020:
+    .strn "插入卡带。{结束}"
+mes_data_0022_0021:
+    .strn "１号存档记录可能损坏{↙}"
+    .strn "无法读取。{↙}"
+    .strn "{结束}"
+mes_data_0022_0022:
+    .strn "消除１号存档记录。{结束}"
+mes_data_0022_0023:
+    .strn "２号存档记录可能损坏{↙}"
+    .strn "无法读取。{↙}"
+    .strn "{结束}"
+mes_data_0022_0024:
+    .strn "消除２号存档记录。{结束}"
+mes_data_0022_0025:
+    .strn "数据无法读取。{结束}"
+mes_data_0022_0026:
+    .strn "Ｗｉ－Ｆｉ设定{结束}"
+mes_data_0022_0027:
+    .strn "Ｗｉ－Ｆｉ设定{结束}"
+mes_data_0022_0028:
+    .strn "Ｗｉ－Ｆｉ设定{结束}"
+mes_data_0022_0029:
+    .strn "Ｗｉ－Ｆｉ设定{结束}"
+mes_data_0022_0030:
+    .strn "Ｗｉ－Ｆｉ设定{结束}"
+mes_data_0022_0031:
+    .strn "１号存档记录可能损坏{结束}"
+mes_data_0022_0032:
+    .strn "无法读取。{结束}"
+mes_data_0022_0033:
+    .strn "消除１号存档记录。{结束}"
+mes_data_0022_0034:
+    .strn "２号存档记录可能损坏{结束}"
+mes_data_0022_0035:
+    .strn "无法读取。{结束}"
+mes_data_0022_0036:
+    .strn "消除２号存档记录。{结束}"
+mes_data_0022_0037:
+    .strn "还无法进行通信的文件…{结束}"
+mes_data_0022_0038:
+    .strn "　没有数据的文件　{结束}"
 
 
 ;文件尾：控制符，子文件内的文本数
